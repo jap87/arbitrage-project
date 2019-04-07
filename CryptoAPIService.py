d1e28ecd5f008704c36b804cc3bf0ba3b900acc7
Juan Philippe <jap87@duke.edu>
2019-04-07 18:20:37 -0400

    basic request to crypto API, no rates yet

---
+++ CryptoAPIService.py
@@ -0,0 +1,13 @@
+import http.client
+import json
+
+connection = http.client.HTTPSConnection('api.nomics.com')
+connection.request("GET", "/v1/markets?key=fd24ac27f03b5016a3a44e8179d6da86")
+response = connection.getresponse()
+# the /markets API route returns a list of markets that this API keeps track of.
+# this line takes the response from the server and turns it into a python list
+list_of_markets = json.loads(response.read())
+print(list_of_markets[0])
+print(list_of_markets[1])
+
+connection.close()
