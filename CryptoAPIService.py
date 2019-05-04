189bcda7b59211f9cfb1cfd403d94c445f093964
Julian Henry <julianplushenry@gmail.com>
2019-05-03 20:34:13 -0400

    Optimizing Neo4J with Python cycle detection 1

--- CryptoAPIService.py
+++ CryptoAPIService.py
@@ -2,37 +2,67 @@ import http.client
 import json
 from flask import Flask
 import time
-from neo4j import GraphDatabase
+import hashlib
+import math
 from py2neo import Graph, Node, Relationship
 
-graph = Graph("bolt://localhost:7687", auth=("neo4j", "juanjulian"))
-tx = graph.begin()
-graph.schema.create_uniqueness_constraint("Currency","name")
+from dag_detect import simple_cycles
 
-# app = Flask(__name__)
-# time.sleep(5)
-# print("hello")
+# graph = Graph("bolt://vcm-8793.vm.duke.edu:7687", auth=("neo4j", "juanjulian"))
+graph = Graph("bolt://vcm-8793.vm.duke.edu:7687", auth=("neo4j", "juanjulian"))
+tx = graph.begin()
+#graph.schema.create_uniqueness_constraint("Currency","name")
+
 # check out http://docs.nomics.com for the list of routes and what they give you and what parameters they require
 connection = http.client.HTTPSConnection('api.nomics.com')
 connection.request("GET", "/v1/exchange-markets/prices?key=fd24ac27f03b5016a3a44e8179d6da86")
 response = connection.getresponse()
 list_of_exchange = json.loads(response.read())
 
-print(list_of_exchange[0])
-unique_names = set()
-for i in list_of_exchange:
-    first_node = graph.nodes.match("Currency", name=i["base"]).first()
-    second_node = graph.nodes.match("Currency", name=i["quote"]).first()
-    relationship = Relationship(first_node,"Buys",second_node,cost=i["price_quote"],timestamp=i["timestamp"])
-    tx.create(relationship)
+unique_names = dict()
+unique_bases = set()
+G = dict()
 
-tx.commit()
-# first_node = Node("Currency",name=list_of_exchange[0]["base"])
-# tx.create(first_node)
-# second_node = Node("Currency",name=list_of_exchange[0]["quote"])
-# tx.create(second_node)
-# relationship = Relationship(first_node,"Buys",second_node,cost=list_of_exchange[0]["price_quote"],timestamp=list_of_exchange[0]["timestamp"])
-# tx.create(relationship)
+#TODO: Remove duplicate edges
+counter = 0
+for i in list_of_exchange:
+    if(i["quote"] not in unique_names.keys()):
+        unique_names[i["quote"]] = counter
+        counter += 1
+    if(i["base"] in unique_bases):
+        G[unique_names[i["base"]]].append(unique_names[i["quote"]])
+    else:
+        if(i["base"] not in unique_names.keys()):
+            unique_names[i["base"]] = counter
+            unique_bases.add(i["base"])
+            counter += 1
+        else:
+            unique_bases.add(i["base"])
+        G[unique_names[i["base"]]] = [unique_names[i["quote"]]]
+
+print(G)
+filtered_list = []
+
+
+# for i in unique_names:
+#     node = Node("Currency",name=i)
+#     tx.create(node)
 # tx.commit()
+
+
+print(tuple(simple_cycles(G)))
+
+# for i in filtered_list:
+#     first_node = graph.nodes.match("Currency", name=i["base"]).first()  
+#     second_node = graph.nodes.match("Currency", name=i["quote"]).first()
+#     print(first_node, second_node)
+#     relationship = Relationship(first_node,"Buys",second_node,cost=math.log(float(i["price_quote"])),timestamp=i["timestamp"])
+#     tx.create(relationship)
+# tx.commit()
+
+# arbitrage = graph.run("MATCH p=(n)-[r1:DISTANCE]->()-[r2:DISTANCE]->()-[r3:DISTANCE]->(n) WHERE r1.dist + r2.dist+r3.dist <> 0 RETURN p").to_subgraph().relationships
+# for i in arbitrage:
+#     print(str(i,-2))
+# S = set()
 
 connection.close()
